90284ac252408d9f8abd438f645bf30f98653c02
adityalochan <aditya.lochan25@gmail.com>
2022-07-03 22:38:14 -0400

    Resolved reload issue

--- designs.js
+++ designs.js
@@ -1,45 +1,40 @@
-// debugger;
-
-// REFERENCES
-// https://stackoverflow.com/questions/14643617/create-table-using-javascript
-// https://stackoverflow.com/questions/27044956/javascript-change-the-color-of-current-element-by-clck
 const sizePicker = document.getElementById('sizePicker');
-const height = document.getElementById('inputHeight').value;
-const width = document.getElementById('inputWidth').value;
-const color = document.getElementById('colorPicker').value;
+const height = document.getElementById('inputHeight');
+const width = document.getElementById('inputWidth');
+const color = document.getElementById('colorPicker');
 
 function respondToTheClick(evt) {
-	evt.target.style.backgroundColor = color;
+	evt.target.style.backgroundColor = color.value;
 }
 
-function makeGrid(height,width) {
+function makeGrid(height, width) {
 
 	console.log("Height:" + height + " width:" + width + " Color: " + color);
 
-  var myTableDiv = document.getElementById('pixelCanvas');
-	var table = document.createElement('TABLE');
-	table.border = '2';
+	var myTableDiv = document.getElementById('pixelCanvas');
 
+	myTableDiv.innerHTML = "";
 	var tableBody = document.createElement('TBODY');
-	table.appendChild(tableBody);
 
-	for (var i = 0; i < height; i++) {
+
+	for (var i = 0; i < height.value; i++) {
 		var tr = document.createElement('TR');
 		tableBody.appendChild(tr);
 
-		for (var j = 0; j < width; j++) {
+		for (var j = 0; j < width.value; j++) {
 			var td = document.createElement('TD');
-			td.width = '75';
-			td.appendChild(document.createTextNode("   "));
+
 			tr.appendChild(td);
 		}
 	}
 	myTableDiv.addEventListener('click', respondToTheClick);
-	myTableDiv.appendChild(table);
+	// myTableDiv.appendChild(table);
+	myTableDiv.appendChild(tableBody);
 }
 
-sizePicker.addEventListener('click', function (event) {
-  event.preventDefault();
+sizePicker.addEventListener('submit', function (event) {
+	event.preventDefault();
+	makeGrid(height, width)
 });
 
-sizePicker.addEventListener("submit", makeGrid(height,width));
+// sizePicker.addEventListener("submit", makeGrid(height,width));
